6ba7fe931dc0bd7173c2eef8bc252651ad964888
Giovanny Suarez <suarezkrac@gmail.com>
2014-02-14 12:19:40 -0500

    Nuevos Archivos
    Nuevos Archivos

--- Clase_9/Ejercicio Login/js/main.js
+++ Clase_9/Ejercicio Login/js/main.js
@@ -1,6 +1,7 @@
 $(document).on('ready', paginaCargada);
-var paginas = 3;
+var paginas = 4;
 var contador = 1;
+var contador_atras = 0;
 
 function paginaCargada () {
 
@@ -9,47 +10,114 @@ function paginaCargada () {
 
 	$('button').on('click', ingresar);
 	$('#next').on('click', siguiente);
+
+	$('#back').on('click', atras);
 }
 function siguiente () {
+	$('#back').show();
+	console.log('adelante: '+contador);
 	
 	if (contador<= paginas) {
 		$('.page_new').load('pagina'+contador+'.html', function(){
+			 /* Agregue*/
+			$('#container').css({
+				'background': 'url(img/fondo2.jpg)',
+				'background-size': 'cover'
+			});
+			 /*aqui*/
+
 			$('.principal').addClass('desplazar-izquierda');
 			$('.principal').addClass('oculto-izquierda');
 			$(this).addClass('desplazar-centro');
 			$(this).addClass('principal');
+			$('#next').off('click', siguiente);
+
 			setTimeout(function(){
 				$('.oculto-izquierda').removeClass('principal');
 				$('.oculto-izquierda').addClass('page_new');
 				$('.page_new').removeClass('oculto-izquierda');
 				$('.page_new').removeClass('desplazar-izquierda');
-			}, 1000);
-			setTimeout(function(){
 				$('.principal').removeClass('page_new');
 				$('.principal').removeClass('desplazar-centro');
+				$('#next').on('click', siguiente);
+				
+			}, 1000);
 
-			},2000);
 		});
 
 		contador++;
 	}
-	if (contador==4){
+	if (contador==5){
 		$('#next').hide();
 	};
 }
-function ingresar () {
-	var nombre = $('#user').val();
-	var contrasena = $('#pass').val();
+function animar_atras(){
+	
+	$('.principal').addClass('desplazar-derecha');
+	$('.principal').addClass('oculto-derecha');
+	$(this).addClass('desplazar-centro');
+	$(this).addClass('principal');
+	$('#back').off('click', atras);
 
-	if (nombre == "admin" && contrasena=="123") {
-		$('#user_login').html(nombre);
-		$('#background_lightbox').removeClass('mostrar');
-		$('#lightbox').removeClass('mostrar');
-		$('#main').addClass('mostrar');
-		$('#next').addClass('mostrar');
+	setTimeout(function(){
+		$('.oculto-derecha').removeClass('principal');
+		$('.oculto-derecha').addClass('page_old');
+		$('.page_old').removeClass('oculto-derecha');
+		$('.page_old').removeClass('desplazar-derecha');
+		$('.principal').removeClass('page_old');
+		$('.principal').removeClass('desplazar-centro');
+		$('#back').on('click', atras);
+		
+	}, 1000);
+}
+
+function atras () {
+	if (contador<5){
+		$('#next').show();
+	};
+
+
+	contador_atras= contador-2;
+
+	console.log('atras: '+contador_atras);
+
+	if (contador_atras==0) {
+		$('.page_old').load('home.html', animar_atras); 
+		$('#container').css({
+			'background': 'url(img/fondo1.jpg)',
+			'background-size': 'cover'
+		});
+		$('#back').hide();
 
 	}else{
-		alert("Usuario o Contraseña no valido");
+		$('.page_old').load('pagina'+contador_atras+'.html', animar_atras);
+
 	}
-		
+
+	contador--;
+
+
+}
+
+function ingresar () {
+
+	$('#main').load('home.html', function(){
+		/*ESTO YA TENIAMOS*/
+		var nombre = $('#user').val();
+		var contrasena = $('#pass').val();
+
+		if (nombre == "admin" && contrasena=="123") {
+			$('#user_login').html(nombre);
+			$('#background_lightbox').removeClass('mostrar');
+			$('#lightbox').removeClass('mostrar');
+			$('#main').addClass('mostrar');
+			$('#back').addClass('mostrar');
+			$('#next').addClass('mostrar');
+			$('#back').hide();
+
+		}else{
+			alert("Usuario o Contraseña no valido");
+		}
+		/*HASTA AQUI*/
+	});
 }
