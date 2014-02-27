e0313ee57c8a2f4a4448fff4cddc0d2c38a28e9f
Giovanny Suarez <suarezkrac@gmail.com>
2014-02-27 11:03:08 -0500

    nuevos archivos
    nuevos archivos

--- Clase_19 (jQuery Mobile)/Mi primera App/js/main.js
+++ Clase_19 (jQuery Mobile)/Mi primera App/js/main.js
@@ -1,1 +1,16 @@
+$(document).on("ready", Listo);
+function Listo () {
+	// body...
+	$('#ingresar_servicio').on('click', serviciosPage);
+}
 
+function serviciosPage () {
+	// body...
+
+	$('.servicios-icon').addClass('animated slideInLeft');
+
+	$('.ui-block-b').addClass('animated lightSpeedIn');
+	
+
+
+}
