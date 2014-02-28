b7b89f879c211a669a996fa53b12e9c3b93eba8d
Giovanny Suarez <suarezkrac@gmail.com>
2014-02-28 09:17:46 -0500

    nuevo
    nuevo

--- Clase_19 (jQuery Mobile)/Mi primera App/js/main.js
+++ Clase_19 (jQuery Mobile)/Mi primera App/js/main.js
@@ -2,6 +2,23 @@ $(document).on("ready", Listo);
 function Listo () {
 	// body...
 	$('#ingresar_servicio').on('click', serviciosPage);
+
+	$('#enviar_mail').on('click', enviarDatosMail);
+}
+
+function enviarDatosMail () {
+	var nombre = $('#name').val();
+	var correo = $('#mail').val();
+	var mensaje = $('#mensaje').val();
+
+	console.log(nombre);
+	console.log(correo);
+	console.log(mensaje);
+
+	$.post("php/enviarMail.php",{nombrePHP: nombre, correoPHP: correo, mensajePHP: mensaje}, function () {
+		alert("Gracias por contactarnos!!..");
+	});
+
 }
 
 function serviciosPage () {
@@ -11,6 +28,8 @@ function serviciosPage () {
 
 	$('.ui-block-b').addClass('animated lightSpeedIn');
 	
-
+	$('.servicios-icon, .ui-block-b').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+		$(this).removeClass('animated slideInLeft lightSpeedIn');
+	});
 
 }
